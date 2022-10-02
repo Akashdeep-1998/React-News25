dd14b8a9897c05c4c939b53ef29ee655cefa2649
Akash <akashdeep.lpu@outlook.com>
2022-10-03 01:46:46 +0530

    add frontend and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="bg-light text-center text-lg-start">
+          <div
+            className="text-center p-3 bg-dark text-white">
+                made with love!!
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
